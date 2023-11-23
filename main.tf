e6093a5037a7ad340ca7d7631d9e16e3b634e56d
Tiriel-Alyptus <lorenzo.decaudin@gmail.com>
2023-11-23 11:30:06 +0100

    up secretize

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-
+    provider "aws" {
+      region = "eu-west-3"
+      access_key = "*"
+      secret_key = "*"
+    }
+    
+    resource "aws_instance" "DC3-HK-T3" {
+      ami           = "ami-0302f42a44bf53a45"
+      instance_type = "t2.micro"
+    }
